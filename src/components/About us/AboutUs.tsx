84534e8860dd544f8e332217157bf266e4f2b46d
Saad <saad.hk46@gmail.com>
2025-01-30 17:24:14 +0500

    Resolved merge conflict in AboutUs.tsx

--- src/components/About us/AboutUs.tsx
+++ src/components/About us/AboutUs.tsx
@@ -1,5 +1,6 @@
 import { Welcome } from "@/components/SvgComponent";
 import Readmore from "@/components/Button/Readmore";
+
 const AboutUs = () => {
   return (
     <div
@@ -9,14 +10,14 @@ const AboutUs = () => {
       <div className="flex justify-center mb-6 md:mb-0 ">
         <Welcome className="text-secondary object-cover pr-6 md:pr-0 " />
       </div>
-      <div className="flex flex-col space-y-4  md:max-w-md ">
-        <span className="text-primary uppercase font-bold  text-center sm:text-left -mt-4 md:text-left">
+      <div className="flex flex-col space-y-4 md:max-w-md ">
+        <span className="text-primary uppercase font-bold text-center sm:text-left -mt-4 md:text-left">
           About Us
         </span>
         <span className="font-bold text-2xl text-center md:text-left">
           WELCOME TO LOREM EXPORTS
         </span>
-        <p className="md:text-lg font-light leading-2 text-justify md:max-w-md  md:text-left">
+        <p className="md:text-lg font-light leading-2 text-justify md:max-w-md md:text-left">
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
@@ -30,4 +31,5 @@ const AboutUs = () => {
     </div>
   );
 };
+
 export default AboutUs;
