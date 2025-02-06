9031d01b5d74c59f78d70c67238b5cf1d261602b
Saad <saad.hk46@gmail.com>
2025-02-06 14:21:39 +0500

    change the order to reverse

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Arrow from "@public/arrow_circle_right.svg";
-
+import {motion} from "motion/react"
 interface ReadmoreProps {
   className?: string;
   label?: string;
@@ -12,7 +12,11 @@ const Readmore: React.FC<ReadmoreProps> = ({
   label = "Read More",
 }) => {
   return (
-    <div className={`relative inline-block md:mt-3 ${className}`}>
+    <motion.div
+    initial={{ scale: 0 }}
+     animate={{ scale: 1 }}
+     transition={{duration:1, delay:0.5}}
+    className={`relative inline-block md:mt-3 ${className}`}>
       <div className={`group`}>
         <div className="absolute inset-0 bg-black translate-x-2 translate-y-2 transition-transform duration-500 group-hover:translate-x-2 group-hover:translate-y-2 group-hover:scale-105 opacity-100" />
         <button
@@ -23,7 +27,7 @@ const Readmore: React.FC<ReadmoreProps> = ({
           <Image src={Arrow} alt="Arrow" width={16} height={16} />
         </button>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
--- src/components/Hero section/HeroSection.tsx
+++ src/components/Hero section/HeroSection.tsx
@@ -3,23 +3,23 @@ import Readmore from "@/components/Button";
 
 const HeroSection = () => {
   return (
-    <div className="flex flex-col md:flex-row md:-mt-6 items-center md:justify-between max-w-screen-lg mx-auto bg-white px-4 md:px-8">
-      <div className="flex order-2 md:order-1 flex-col space-y-4 font-lexend max-w-md ">
+    <div className="flex flex-col-reverse md:flex-row md:-mt-6 items-center md:justify-between max-w-screen-lg mx-auto bg-white px-4 md:px-8">
+      <div className="flex flex-col space-y-4 font-lexend max-w-md">
         <span className="text-primary font-medium text-sm uppercase">
           Transport & Logistics Solutions
         </span>
-        <h1 className="font-bold text-2xl sm:text-2xl md:text-3xl text-gray-900 md:leading-snug leading-tight ">
+        <h1 className="font-bold text-2xl sm:text-2xl md:text-3xl text-gray-900 md:leading-snug leading-tight">
           RIGHT PRICE AT RIGHT PLACE. OUR CORE VALUES ARE TRUST, TALENT AND
           PASSION
         </h1>
-        <p className="text-secondary font-bold uppercase max-w-sm ">
+        <p className="text-secondary font-bold uppercase max-w-sm">
           We are transforming challenges into success.
         </p>
         <div className="flex justify-center md:justify-start">
           <Readmore className="bg-primary" />
         </div>
       </div>
-      <div className="order-1 md:order-2 flex justify-end">
+      <div className="flex justify-end">
         <Transport className="text-primary object-cover" />
       </div>
     </div>
