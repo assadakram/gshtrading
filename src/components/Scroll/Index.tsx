f5553b1a510d79ffe4ee2d5b80330e9c8bb64686
Saad <saad.hk46@gmail.com>
2025-02-07 17:27:28 +0500

    changes

--- src/components/Scroll/Index.tsx
+++ src/components/Scroll/Index.tsx
@@ -4,17 +4,18 @@ import React, { useEffect, useState } from "react";
 import { FaArrowUp, FaArrowDown } from "react-icons/fa";
 
 const ScrollToTopArrow: React.FC = () => {
+  const [scrollDirection, setScrollDirection] = useState<"up" | "down">("down");
   const [isVisible, setIsVisible] = useState<boolean>(false);
-  const [scrollDirection, setScrollDirection] = useState<"up" | "down">("up");
+
   useEffect(() => {
     const handleScroll = () => {
-      const scrollPosition =
+      const currentScrollPos =
         window.scrollY || document.documentElement.scrollTop;
-      const maxScroll =
-        document.documentElement.scrollHeight - window.innerHeight;
-      setIsVisible(scrollPosition > 300 || scrollPosition < maxScroll - 300);
-      setScrollDirection(scrollPosition < maxScroll - 300 ? "down" : "up");
+      const halfScreen = document.documentElement.scrollHeight / 3;
+      setScrollDirection(currentScrollPos > halfScreen ? "up" : "down");
+      setIsVisible(currentScrollPos > 150);
     };
+
     window.addEventListener("scroll", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
@@ -35,17 +36,15 @@ const ScrollToTopArrow: React.FC = () => {
   return (
     <button
       onClick={handleClick}
-      className={`fixed bottom-8 right-8  ${
-        isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-4"
-      }  bg-primary text-white w-12 h-12 flex items-center justify-center rounded-md shadow-md transition-all duration-300`}
-      aria-label={`Scroll ${
-        scrollDirection === "up" ? "to top" : "to bottom"
-      } `}
+      className={`fixed bottom-8 right-8 bg-primary text-white w-12 h-12 flex items-center justify-center rounded-md shadow-md transition-all duration-300 ${
+        isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
+      }`}
+      aria-label={`Scroll ${scrollDirection === "up" ? "to top" : "to bottom"}`}
     >
       {scrollDirection === "up" ? (
-        <FaArrowUp className="w-5 h-5" />
+        <FaArrowUp />
       ) : (
-        <FaArrowDown className="w-5 h-5" />
+        <FaArrowDown />
       )}
     </button>
   );
